8660fe3d55089ee109daa1af91012175a37a2e24
Tom Wu <80329793+Wentong-Wu@users.noreply.github.com>
2022-08-10 17:11:37 +0100

    Port 2000

--- src/main/java/com/org/training/wentongwu/springboot/demo/DemoApplication.java
+++ src/main/java/com/org/training/wentongwu/springboot/demo/DemoApplication.java
@@ -7,9 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @SpringBootApplication
 @RestController
 public class DemoApplication {
