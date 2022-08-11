b708d6714bcaaf9274cf2163e2c3dd81e4f5ee76
Tom Wu <80329793+Wentong-Wu@users.noreply.github.com>
2022-08-11 14:26:52 +0100

    added bat testing

--- src/test/java/com/org/training/wentongwu/springboot/demo/DemoUnfinishedTest.java
+++ src/test/java/com/org/training/wentongwu/springboot/demo/DemoUnfinishedTest.java
@@ -1,5 +1,7 @@
 package com.org.training.wentongwu.springboot.demo;
 
+import io.cucumber.java.zh_tw.並且;
+import io.cucumber.java.zh_tw.但是;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -31,5 +33,30 @@ public class DemoUnfinishedTest {
     public void test_animal_poop(){
         Cat cat = new Cat("Tom",100,true,true,10);
         assertEquals("Poop",cat.Poop());
+    }
+    @Test
+    public void test_bat_scream(){
+        Bat bat = new Bat("Vamp",100,true,true);
+        assertEquals("Scream",bat.scream());
+    }
+    @Test
+    public void test_bat_eat(){
+        Bat bat = new Bat("Vamp",100,true,true);
+        assertEquals("Bat eat",bat.eat());
+    }
+    @Test
+    public void test_bat_takeOff(){
+        Bat bat = new Bat("Vamp",100,true,true);
+        assertEquals("bat take off", bat.takeOff());
+    }
+    @Test
+    public void test_bat_flying(){
+        Bat bat = new Bat("Vamp",100,true,true);
+        assertEquals("bat flying",bat.flying());
+    }
+    @Test
+    public void test_bat_landing(){
+        Bat bat = new Bat("Vamp",100,true,true);
+        assertEquals("bat landing",bat.landing());
     }
 }
