ce485035673421990ed3cc7da043dd6ee83b0b0d
Tom Wu <80329793+Wentong-Wu@users.noreply.github.com>
2022-08-11 11:40:42 +0100

    remove the protected in test

--- src/test/java/com/org/training/wentongwu/springboot/demo/DemoApplicationTests.java
+++ src/test/java/com/org/training/wentongwu/springboot/demo/DemoApplicationTests.java
@@ -12,44 +12,44 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class DemoApplicationTests {
 
 	@Test
-	protected void test_get_cat_name(){
+	void test_get_cat_name(){
 		Cat testCat = new Cat("Timmy",100, true, true,10);
 		assertEquals("Timmy",testCat.getName(),"getName method failed");
 	}
 
 	@Test
-	protected void test_set_cat_name(){
+	void test_set_cat_name(){
 		Cat testCat = new Cat("Timmy",100, true, true,10);
 		testCat.setName("Tommy");
 		assertEquals("Tommy",testCat.getName(),"setName method failed");
 	}
 	@Test
-	protected void test_set_hair(){
+	void test_set_hair(){
 		Cat testCat = new Cat("Timmy",100, true, true,10);
 		testCat.setHair(false);
 		assertEquals(false,testCat.isHair());
 	}
 	@Test
-	protected void test_is_hair(){
+	void test_is_hair(){
 		Cat testCat = new Cat("Timmy",100, true, true,10);
 		assertEquals(true,testCat.isHair());
 	}
 
 	@Test
-	protected void test_set_cat_power(){
+	void test_set_cat_power(){
 		Cat testCat = new Cat("Timmy",100, true, true,10);
 		testCat.setDamagePower(20);
 		assertEquals(20,testCat.getDamagePower());
 	}
 
 	@Test
-	protected void test_get_cat_power(){
+	void test_get_cat_power(){
 		Cat testCat = new Cat("Timmy",100, true, true,10);
 		assertEquals(10,testCat.getDamagePower());
 	}
 
 	@Test
-	protected void test_attack(){
+	void test_attack(){
 		Cat testCat = new Cat("Tim",100,true,true,10);
 		Bat testBat = new Bat("Vlad",100,true,true);
 		testBat.setHealth(100);
@@ -57,46 +57,46 @@ class DemoApplicationTests {
 		assertEquals(90,testBat.getHealth(),"Attack not working");
 	}
 	@Test
-	protected void test_isAlive(){
+	void test_isAlive(){
 		Bat testBat = new Bat("vamp",100,true,true);
 		assertEquals(true,testBat.getIsAlive());
 	}
 	@Test
-	protected void test_die(){
+	void test_die(){
 		Bat testBat = new Bat("vamp",100,true,true);
 		testBat.Died();
 		assertEquals(false,testBat.getIsAlive());
 	}
 	@Test
-	protected void test_isMale(){
+	void test_isMale(){
 		Bat testBat = new Bat("vamp",100,true,true);
 		assertEquals(true,testBat.isMale());
 	}
 	@Test
-	protected void test_set_isMale(){
+	void test_set_isMale(){
 		Bat testBat = new Bat("vamp",100,true,true);
 		testBat.setMale(false);
 		assertEquals(false,testBat.isMale());
 	}
 	@Test
-	protected void test_get_health(){
+	void test_get_health(){
 		Bat testBat = new Bat("vamp",100,true,true);
 		assertEquals(100,testBat.getHealth());
 	}
 	@Test
-	protected void test_set_health(){
+	void test_set_health(){
 		Bat testBat = new Bat("vamp",100,true,true);
 		testBat.setHealth(50);
 		assertEquals(50,testBat.getHealth());
 	}
 	@Test
-	protected void test_get_AnimalCollection(){
+	void test_get_AnimalCollection(){
 		AnimalCollection animalCollection = new AnimalCollection();
 		List<Animal> animallength = animalCollection.getAnimalCollection();
 		assertEquals(4,animallength.size());
 	}
 	@Test
-	protected void test_add_animalCollection(){
+	void test_add_animalCollection(){
 		AnimalCollection animalCollection = new AnimalCollection();
 		animalCollection.addAnimalCollection(new Cat("Tim",50,true,true,30));
 		List<Animal> animalList = animalCollection.getAnimalCollection();
