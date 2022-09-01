05b326978e9ae4ad317c29090c81813ed0704279
Tom Wu <80329793+Wentong-Wu@users.noreply.github.com>
2022-09-01 12:30:21 +0100

    0.0

--- src/main/java/com/org/training/wentongwu/springboot/demo/DemoApplication.java
+++ src/main/java/com/org/training/wentongwu/springboot/demo/DemoApplication.java
@@ -1,8 +1,10 @@
 package com.org.training.wentongwu.springboot.demo;
 
+import com.google.gson.JsonObject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @RestController
@@ -11,7 +13,7 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 	public static AnimalCollection animalCollection = new AnimalCollection();
-	@CrossOrigin(origins = "http::localhost:2000")
+	//@CrossOrigin(origins = "http::localhost:8080")
 	@GetMapping("/customRoute")
 	@ResponseBody
 	public String myResponse(){
@@ -23,4 +25,5 @@ public class DemoApplication {
 	public void change(@RequestParam String name, @RequestParam int health){
 		animalCollection.addAnimalCollection(new FlyingRat(name,health));
 	}
+
 }
